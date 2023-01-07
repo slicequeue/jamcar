560b39dc324c56bfb567a9b4b4dda01527de9e92
slicequeue <slicequeue@gmail.com>
2023-01-07 20:27:50 +0900

    chore: 리팩토링 - 프로젝트 명칭으로 변경

--- src/main/java/com/slicequeue/jamcar/Application.java
+++ src/main/java/com/slicequeue/jamcar/Application.java
@@ -1,13 +1,13 @@
-package com.slicequeue.project;
+package com.slicequeue.jamcar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/com/slicequeue/jamcar/ApplicationTests.java
+++ src/test/java/com/slicequeue/jamcar/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.slicequeue.project;
+package com.slicequeue.jamcar;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjectApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
