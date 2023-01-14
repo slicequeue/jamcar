f78703f53dabf465f3570fefe6c3806e4d39ce63
slicequeue <slicequeue@gmail.com>
2023-01-14 17:33:29 +0900

    feat: WebDriverLoader 인터페이스 선언

---
+++ src/main/java/com/slicequeue/jamcar/service/crawling/selenium/driver/WebDriverLoader.java
@@ -0,0 +1,8 @@
+package com.slicequeue.jamcar.service.crawling.selenium.driver;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public interface WebDriverLoader {
+
+    RemoteWebDriver load();
+}
