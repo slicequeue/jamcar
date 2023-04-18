365bd7d1700bd3f9fe2dbcf0afb298b3541a83b1
slicequeue <slicequeue@gmail.com>
2023-04-18 23:30:09 +0900

    feat: 유틸 클래스 작업 - String 패턴 매칭 관련 클래스 추가

---
+++ src/main/java/com/slicequeue/jamcar/common/utils/StringPatternMatchingUtil.java
@@ -0,0 +1,23 @@
+package com.slicequeue.jamcar.common.utils;
+
+import java.util.regex.Pattern;
+
+public final class StringPatternMatchingUtil {
+
+    public static boolean isValidPassword(String password) {
+        // 비밀번호는 8~20자리의 영문, 숫자, 특수문자 조합이어야 함
+        String passwordRegex = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$";
+        Pattern pattern = Pattern.compile(passwordRegex);
+        return pattern.matcher(password).matches();
+    }
+
+    public static boolean isValidEmail(String email) {
+        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        return pattern.matcher(email).matches();
+    }
+
+    private StringPatternMatchingUtil() {
+        throw new IllegalStateException("no StringPatternMatchingUtil");
+    }
+}
