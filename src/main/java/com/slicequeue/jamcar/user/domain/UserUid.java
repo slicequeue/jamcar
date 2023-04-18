c6a0602383ab8f4df47b25db43ba1828a007ad93
slicequeue <slicequeue@gmail.com>
2023-04-18 23:30:39 +0900

    feat: VO 클래스 작업 - Email, UserUid, Password

---
+++ src/main/java/com/slicequeue/jamcar/user/domain/UserUid.java
@@ -0,0 +1,42 @@
+package com.slicequeue.jamcar.user.domain;
+
+import org.hibernate.annotations.Comment;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+@Embeddable
+public class UserUid implements Serializable {
+
+    @Comment("사용자 식별값")
+    @Column(columnDefinition = "BINARY(16)", updatable = false)
+    private final UUID uid;
+
+    public UserUid(UUID uid) {
+        this.uid = uid;
+    }
+
+    public UserUid() {
+        this.uid = UUID.randomUUID();
+    }
+
+    public UUID getUid() {
+        return uid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserUid userUid = (UserUid) o;
+        return Objects.equals(uid, userUid.uid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uid);
+    }
+}
