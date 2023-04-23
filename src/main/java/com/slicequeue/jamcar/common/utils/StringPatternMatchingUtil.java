6b0f881a9e11f8f93fa46019376f0c0bf0103b29
slicequeue <slicequeue@gmail.com>
2023-04-23 22:52:41 +0900

    feat: Util 클래스 작업 - ReflectUtil 추가 & StringPatternMatchingUtil 열거형으로 리팩토링

--- src/main/java/com/slicequeue/jamcar/common/utils/StringPatternMatchingUtil.java
+++ src/main/java/com/slicequeue/jamcar/common/utils/StringPatternMatchingUtil.java
@@ -1,21 +1,32 @@
 package com.slicequeue.jamcar.common.utils;
 
-import org.springframework.util.StringUtils;
-
 import java.util.regex.Pattern;
 
 public final class StringPatternMatchingUtil {
 
+    public enum RegexPattern {
+        passwordValidRegexLv3("비밀번호 체크 Lv3 8~20자리의 영문, 숫자, 특수문자 조합",
+                "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$"),
+        emailValidRegex("이메일 주소 형식", "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"),
+
+        ;
+
+        private final String description;
+        private final String regex;
+
+        RegexPattern(String description, String regex) {
+            this.description = description;
+            this.regex = regex;
+        }
+    }
+
     public static boolean isValidPassword(String password) {
-        // 비밀번호는 8~20자리의 영문, 숫자, 특수문자 조합이어야 함
-        String passwordRegex = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$";
-        Pattern pattern = Pattern.compile(passwordRegex);
+        Pattern pattern = Pattern.compile(RegexPattern.passwordValidRegexLv3.regex);
         return pattern.matcher(password).matches();
     }
 
     public static boolean isValidEmail(String email) {
-        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
-        Pattern pattern = Pattern.compile(emailRegex);
+        Pattern pattern = Pattern.compile(RegexPattern.emailValidRegex.regex);
         return pattern.matcher(email).matches();
     }
 
