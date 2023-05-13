f07d98194e7bbb02152b565087309db6bbbf42b8
slicequeue <slicequeue@gmail.com>
2023-05-14 00:34:34 +0900

    feat: 엔티티 모델 작업 - Jamcar & VO 선언 1차
    - 기본 속성 부터 논리 삭제까지 적용
    - VO 적용
      - Address 주소
      - Creator 작성자

---
+++ src/main/java/com/slicequeue/jamcar/jamcar/command/domain/vo/Address.java
@@ -0,0 +1,61 @@
+package com.slicequeue.jamcar.jamcar.command.domain.vo;
+
+import lombok.Builder;
+import org.hibernate.annotations.Comment;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+@Embeddable
+public class Address {
+
+    @Comment("우편번호")
+    @Column(length = 8)
+    private String postalCode;
+
+    @NotNull
+    @Comment("주소")
+    @Column(length = 1024, nullable = false)
+    private String address;
+
+    @Builder
+    public Address(String postalCode, String address) {
+        this.postalCode = postalCode;
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Address address1)) return false;
+        return Objects.equals(getPostalCode(), address1.getPostalCode()) && Objects.equals(getAddress(), address1.getAddress());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getPostalCode(), getAddress());
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "postalCode='" + postalCode + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    void updatePostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+}
