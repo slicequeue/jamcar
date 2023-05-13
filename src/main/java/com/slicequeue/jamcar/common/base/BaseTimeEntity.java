9977b60cedfae0aedbf182d34a12364e99628436
slicequeue <slicequeue@gmail.com>
2023-05-14 00:32:16 +0900

    chore: 엔티티 작업 - BaseTimeEntity Comment 어노테이션 추가

--- src/main/java/com/slicequeue/jamcar/common/base/BaseTimeEntity.java
+++ src/main/java/com/slicequeue/jamcar/common/base/BaseTimeEntity.java
@@ -1,6 +1,7 @@
 package com.slicequeue.jamcar.common.base;
 
 import lombok.Getter;
+import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -16,10 +17,12 @@ import java.time.Instant;
 public class BaseTimeEntity {
 
     @CreationTimestamp
+    @Comment("생성일시")
     @Column(updatable = false)
     private Instant createdAt;
 
     @UpdateTimestamp
+    @Comment("수정일시")
     @Column
     private Instant updatedAt;
 
