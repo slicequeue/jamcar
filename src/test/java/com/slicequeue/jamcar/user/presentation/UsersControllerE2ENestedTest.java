83b3b1b5996fcea52dcb0189f27b2bbc349b1587
slicequeue <slicequeue@gmail.com>
2023-05-16 22:41:07 +0900

    fix: 테스트 작업 - SpringBootTest 데이터베이스 격리(초기화) 를 위해 TRUNCATE 실행 구문 적용

--- src/test/java/com/slicequeue/jamcar/user/presentation/UsersControllerE2ENestedTest.java
+++ src/test/java/com/slicequeue/jamcar/user/presentation/UsersControllerE2ENestedTest.java
@@ -22,9 +22,11 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static com.slicequeue.jamcar.util.ParameterizedTestUtil.getParamStreamArguments;
@@ -38,11 +40,34 @@ class UsersControllerE2ENestedTest {
     @LocalServerPort
     int port;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @BeforeEach
     public void setUp() {
         System.out.println("setUp1");
         RestAssured.port = port;
         System.out.println(RestAssured.port);
+    }
+
+    @AfterAll
+    public void afterAll() {
+        final List<String> truncateQueries = getTruncateQueries(jdbcTemplate);
+        truncateTables(jdbcTemplate, truncateQueries);
+    }
+
+    private List<String> getTruncateQueries(final JdbcTemplate jdbcTemplate) {
+        return jdbcTemplate.queryForList("SELECT Concat('TRUNCATE TABLE ', TABLE_NAME, ';') AS q FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'PUBLIC'", String.class);
+    }
+
+    private void truncateTables(final JdbcTemplate jdbcTemplate, final List<String> truncateQueries) {
+        execute(jdbcTemplate, "SET REFERENTIAL_INTEGRITY FALSE");
+        truncateQueries.forEach(v -> execute(jdbcTemplate, v));
+        execute(jdbcTemplate, "SET REFERENTIAL_INTEGRITY TRUE");
+    }
+
+    private void execute(final JdbcTemplate jdbcTemplate, final String query) {
+        jdbcTemplate.execute(query);
     }
 
     @Nested
