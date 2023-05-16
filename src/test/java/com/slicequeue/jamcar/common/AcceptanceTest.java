528719998e91ae9e6c7ef526fb77073a929b3530
slicequeue <slicequeue@gmail.com>
2023-05-16 22:42:02 +0900

    test: 테스트 작업 - SpringBootTest 데이터베이스 격리(초기화)를 위한 AcceptanceTest 어노테이션 적용

---
+++ src/test/java/com/slicequeue/jamcar/common/AcceptanceTest.java
@@ -0,0 +1,16 @@
+package com.slicequeue.jamcar.common;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Retention(RetentionPolicy.RUNTIME)
+@TestExecutionListeners(value = {
+        AcceptanceTestExecutionListener.class},
+        mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS
+)
+public @interface AcceptanceTest {
+}
