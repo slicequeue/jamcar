ce1fd0143ea866754cc796e54dca4fc3e4c688ae
slicequeue <slicequeue@gmail.com>
2023-06-11 14:28:16 +0900

    feat: 응용서비스 작업 - 잼카 등록 createJamcar 메소드 구현

---
+++ src/main/java/com/slicequeue/jamcar/jamcar/command/application/JamcarService.java
@@ -0,0 +1,34 @@
+package com.slicequeue.jamcar.jamcar.command.application;
+
+import com.slicequeue.jamcar.jamcar.command.domain.CreateJamcarService;
+import com.slicequeue.jamcar.jamcar.command.domain.Jamcar;
+import com.slicequeue.jamcar.jamcar.presentation.JamcarDto;
+import com.slicequeue.jamcar.user.command.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JamcarService {
+
+    private final CreateJamcarService createJamcarService;
+
+    public JamcarDto createJamcar(User user, CreateJamcarRequest createJamcarRequest) {
+        // jamcar 등록 처리
+        Jamcar jamcar = Jamcar.create(createJamcarService, user.getUserUid(), user.getName(), createJamcarRequest);
+
+        // DTO 반환 및 응답
+        return JamcarDto.builder()
+                .id(jamcar.getId())
+                .fromPostalCode(jamcar.getFromAddress().getPostalCode())
+                .fromAddress(jamcar.getFromAddress().getAddress())
+                .toPostalCode(jamcar.getToAddress().getPostalCode())
+                .toAddress(jamcar.getToAddress().getAddress())
+                .creatorName(jamcar.getCreator().getName())
+                .startDate(jamcar.getStartDate())
+                .endDate(jamcar.getEndDate())
+                .status(jamcar.getStatus())
+                .build();
+    }
+
+}
